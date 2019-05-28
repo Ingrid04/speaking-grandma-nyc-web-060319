b3b56050490a0aa16ee17a155a01632cde9832b1
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-28 01:44:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,45 @@
-# Write a speak_to_grandma method.
+greeting = "Hi Nana, how are you?"
+greeting2 ="Hi!"
+shout = "WHAT DID YOU EAT TODAY?"
+repeat = "WHAT?"
+phrase ="I LOVE YOU GRANDMA!"
+shouting = true
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+def speak_to_grandma
+  if speak_to_grandma == greeting
+   print "Huh?! speak up, sonny!".upcase unless shouting
+  elsif speak_to_grandma == greeting2
+   print "Huh?! speak up, sonny!".upcase
+  elsif speak_to_grandma == shout
+   print "no, not since 1938!".upcase
+  elsif speak_to_grandma == repeat
+   print "no, not since 1938!".upcase
+  elsif speak_to_grandma == phrase
+   print "i love you too pumpkin!".upcase
+end
 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
+speak_to_grandma == greeting
 
-# NO, NOT SINCE 1938!
+# if you say I LOVE YOU GRANDMA!, say that she replies I LOVE YOU TOO PUMPKIN!
+if phrase == "I LOVE YOU GRANDMA!"
+  print "i love you too pumpkin!".upcase
+end
+# if you say 'Hi Nana, how are you?', she replies "Huh?! speak up, sonny!".upcase unless shouting
+if greeting == "Hi Nana, how are you?"
+  print "Huh?! speak up, sonny!".upcase unless shouting
+end
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# if you say 'Hi!' for the second time, she replies HUH?! SPEAK UP, SONNY! for a second time'
+if greeting2 == "Hi!"
+  print "Huh?! speak up, sonny!".upcase
+end
+
+# if you say 'WHAT DID YOU EAT TODAY?', she replies NO, NOT SINCE 1938!
+if shout == "WHAT DID YOU EAT TODAY?"
+  print "no, not since 1938!".upcase
+end
+
+# if you say 'WHAT?', she replies NO, NOT SINCE 1938! for a second time.
+#if repeat == "WHAT?"
+  #print "no, not since 1938!".upcase
+#end
