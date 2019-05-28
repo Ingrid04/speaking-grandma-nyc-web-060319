be7d223f4c41456114297b289469d5cc6b6f2a21
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-28 02:10:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,3 @@
-$greeting = "Hi Nana, how are you?"
-$greeting2 ="Hi!"
-$shout = "WHAT DID YOU EAT TODAY?"
-$repeat = "WHAT?"
-$phrase ="I LOVE YOU GRANDMA!"
-$shouting = true
-
 def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
