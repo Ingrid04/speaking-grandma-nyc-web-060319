bfb0a7d6c36d4cec4d6e10b8d22ce1f96f422e3c
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-28 01:51:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,3 +18,28 @@ def speak_to_grandma(input):
    print "i love you too pumpkin!".upcase
 end
 
+#self.speak_to_grandma
+
+# if you say I LOVE YOU GRANDMA!, say that she replies I LOVE YOU TOO PUMPKIN!
+if phrase == "I LOVE YOU GRANDMA!"
+  print "i love you too pumpkin!".upcase
+end
+# if you say 'Hi Nana, how are you?', she replies "Huh?! speak up, sonny!".upcase unless shouting
+if greeting == "Hi Nana, how are you?"
+  print "Huh?! speak up, sonny!".upcase unless shouting
+end
+
+# if you say 'Hi!' for the second time, she replies HUH?! SPEAK UP, SONNY! for a second time'
+if greeting2 == "Hi!"
+  print "Huh?! speak up, sonny!".upcase
+end
+
+# if you say 'WHAT DID YOU EAT TODAY?', she replies NO, NOT SINCE 1938!
+if shout == "WHAT DID YOU EAT TODAY?"
+  print "no, not since 1938!".upcase
+end
+
+# if you say 'WHAT?', she replies NO, NOT SINCE 1938! for a second time.
+#if repeat == "WHAT?"
+  #print "no, not since 1938!".upcase
+#end
